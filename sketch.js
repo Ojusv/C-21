348422058ca1a89c5522c05233eb0981f903ccf9
Ojusv <85628119+Ojusv@users.noreply.github.com>
2021-07-01 12:58:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+let engine;
+let world;
+
+var ground;
+var left;
+var right;
+var top_wall;
+var ball;
+var Vbutton,Hbutton;
+function setup() {
+  createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+  
+  ground =new Ground(200,390,400,20);
+  right = new Ground(390,200,20,400);
+  left = new Ground(10,200,20,400);
+  top_wall = new Ground(200,10,400,20);
+ 
+var ball_options = {
+restitution : 0.01
+
+}
+  ball = Bodies.circle(200,200,10,ball_options);
+ World.add(world,ball);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+
+  Vbutton = createImg("right.png");
+  Vbutton.position(220,30);
+  Vbutton.size(50,50);
+  Vbutton.mouseClicked (Vforce);
+
+  Hbutton = createImg("up.png");
+  Hbutton.position(20,30);
+  Hbutton.size(50,50);
+  Hbutton.mouseClicked (Hforce);
+}
+
+function draw() 
+{
+  background(51);
+  ellipse(ball.position.x,ball.position.y,10);
+  ground.show();
+  top_wall.show();
+  left.show();
+  right.show();
+  Engine.update(engine);
+ 
+
+ 
+  
+}
+function Hforce(){
+Matter.Body.applyForce(ball,{x:0,y:0},{x:0.005,y:0})
+
+}
+function Vforce(){
+Matter.Body.applyForce(ball,{x:0,y:0},{x:0,y:-0.005})
+}
